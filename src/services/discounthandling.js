37f040b2fa1afb3ae5564cea312ce50199abf018
soheil <soheilkooshan710@gmail.com>
2024-01-23 02:44:43 +0330

    some style

--- src/services/discounthandling.js
+++ src/services/discounthandling.js
@@ -3,7 +3,11 @@ import data from "./data";
 async function discountProducts() {
   const allProducts = await data();
   const proArr = await allProducts[0].products;
-  const discountProducts = proArr.filter((res) => res.discount);
+  const discountProducts = proArr
+    .filter((res) => res.discount)
+    .sort((a, b) => {
+      return b.discount - a.discount;
+    });
 
   return discountProducts;
 }
