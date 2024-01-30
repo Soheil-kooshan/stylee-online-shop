73a6cf598ac7f4eda169587d4d69ddefb4a42f7c
soheil <soheilkooshan710@gmail.com>
2024-01-31 00:02:56 +0330

    router defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./style/style.css";
 import React from "react";
 import Landing from "./pages/landing/landing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<p>login</p>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
