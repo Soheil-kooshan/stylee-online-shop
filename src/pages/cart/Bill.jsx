5944bb06d7f15a173906afae40d514686ea55402
soheil <soheilkooshan710@gmail.com>
2024-02-09 20:54:38 +0330

    bill added

--- src/pages/cart/Bill.jsx
+++ src/pages/cart/Bill.jsx
@@ -3,10 +3,10 @@ import styles from "./Bill.module.css";
 function Bill() {
   return (
     <div className={styles.container}>
-      <span className={styles.prePrice}>کل مبلغ :1300000 </span>
-      <span className={styles.discount}>تخفیف کل:140000</span>
-      <span className={styles.total}>مبلغ قابل پرداخت:100000</span>
-      <button className={styles.button}>تایید و پرداخت</button>
+      <span>کل مبلغ :1300000 </span>
+      <span>تخفیف کل:140000</span>
+      <span>مبلغ قابل پرداخت:100000</span>
+      <button>تایید و پرداخت</button>
     </div>
   );
 }
