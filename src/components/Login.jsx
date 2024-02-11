adf22d54677954a5709b6e510f4da03a7e71616b
soheil <soheilkooshan710@gmail.com>
2024-02-11 13:51:57 +0330

    login box created

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,24 @@
 import styles from "./Login.module.css";
 function Login() {
   return (
-    <div className={styles.login}>
-      <p> ورود</p>
-      <img
-        src="src/assets/icons/login_FILL0_wght400_GRAD0_opsz24.svg"
-        alt="login"
-      />
-    </div>
+    <>
+      <div className={styles.loginIcon}>
+        <p> ورود</p>
+        <img
+          src="src/assets/icons/login_FILL0_wght400_GRAD0_opsz24.svg"
+          alt="login"
+        />
+      </div>
+      <div className={styles.loginBox}>
+        <p className={styles.welcome}>خوش آمدید</p>
+
+        <label>شماره همراه</label>
+        <input type="text" />
+        <label>رمز</label>
+        <input type="password" minLength={6} />
+        <p className={styles.signUpCheck}>ثبت نام نکرده ام</p>
+      </div>
+    </>
   );
 }
 
