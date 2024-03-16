167d5c19b13687b54aaf67205d711e53658c2626
soheil <soheilkooshan710@gmail.com>
2024-03-17 01:03:31 +0330

    statics changed to dynamics

--- src/pages/PoroductList/ProductList.jsx
+++ src/pages/PoroductList/ProductList.jsx
@@ -1,18 +1,23 @@
+import { useSelector, useDispatch } from "react-redux";
+import { fetchData } from "../../store/productsSlice";
+import { useEffect } from "react";
 import Product from "./Product";
 import styles from "./ProductList.module.css";
 
 function ProductList() {
+  const array = useSelector((state) => state.products.data);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      {array.products?.map((product) => {
+        return <Product prod={product} key={product.id} />;
+      })}
     </div>
   );
 }
