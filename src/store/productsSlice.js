01986403e13e0dc55e1bdae5ef856e189845f839
soheil <soheilkooshan710@gmail.com>
2024-03-17 01:01:50 +0330

    slice fixed

--- src/store/productsSlice.js
+++ src/store/productsSlice.js
@@ -1,21 +1,20 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import data from "../services/data";
 
-initialState = { data: null };
-
-const fetchData = createAsyncThunk("data/fetch", async () => {
-  const res = await data;
+export const fetchData = createAsyncThunk("data/fetch", async () => {
+  const res = await fetch("src/products.json");
   const finalData = await res.json();
-
   return finalData;
 });
 
-const productsSlice = createSlice({
+const productsReducer = createSlice({
   name: "products",
-  initialState,
-  extraReducers: {
-    [fetchData.fulfilled]: (state) => {
-      state.data = state.payload;
-    },
-  },
+  initialState: { data: [] },
+
+  extraReducers: (builder) =>
+    builder.addCase(fetchData.fulfilled, (state, action) => {
+      state.data = action.payload;
+    }),
 });
+
+export default productsReducer.reducer;
