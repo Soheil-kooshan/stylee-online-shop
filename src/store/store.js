58e44e748d74e3572a9005bd156d8609561da93a
soheil <soheilkooshan710@gmail.com>
2024-04-20 21:29:16 +0330

    cart slice connected

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./productsSlice";
+import cartReducer from "./cartSlice";
 
 const store = configureStore({
   reducer: {
     products: productReducer,
+    cart: cartReducer,
   },
 });
 
