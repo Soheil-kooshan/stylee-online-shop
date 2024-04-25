04bc7c706d035add73c39d2f7057c627464cacd8
soheil <soheilkooshan710@gmail.com>
2024-04-26 00:49:00 +0330

    deleting item added

--- src/pages/cart/CartItem.jsx
+++ src/pages/cart/CartItem.jsx
@@ -1,19 +1,31 @@
 import styles from "./CartItem.module.css";
+import { useDispatch } from "react-redux";
+import { RemoveAction } from "../../store/cartSlice";
 
-function CartItem({ name, image, price, discount }) {
+function CartItem({ item, name, image, price, discount, id }) {
   console.log(name);
+
+  const dispatch = useDispatch();
   return (
     <div className={styles.container}>
       <div>
         <img className={styles.image} src={image} alt="photo" />
       </div>
       <div className={styles.infoBox}>
-        <span className={styles.name}>{name}</span>
+        <span className={styles.name}>
+          {name} کد {id}
+        </span>
         <span className={styles.discount}>
           تخفیف: {discount == null ? "ندارد" : `%${discount}`}
         </span>
         <span className={styles.price}>قیمت : {price}</span>
       </div>
+      <img
+        className={styles.delete}
+        src="src\assets\icons\delete_FILL0_wght400_GRAD0_opsz24.svg"
+        alt="delete"
+        onClick={() => dispatch(RemoveAction(item))}
+      />
     </div>
   );
 }
