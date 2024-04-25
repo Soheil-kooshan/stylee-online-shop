1bd883356a58ee51d8be2573269d79285be7e8b6
soheil <soheilkooshan710@gmail.com>
2024-04-26 00:50:53 +0330

    cart icon status fixed

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -4,15 +4,18 @@ import Menu from "../components/Menu";
 import Logo from "../components/logo";
 import Login from "../components/Login";
 import GoToCart from "../components/GoToCart";
+import { useLocation } from "react-router-dom";
 
 function Navbar() {
+  const location = useLocation().pathname;
+  console.log(location);
   return (
     <>
       <div className={styles.navbar}>
         <Logo />
         <Search />
         <Login />
-        <GoToCart />
+        {location !== "/cart" && <GoToCart />}
         <Menu />
       </div>
     </>
