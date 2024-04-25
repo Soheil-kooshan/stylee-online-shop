58d4df148bd98606c77462b863e43c0d1fe34e42
soheil <soheilkooshan710@gmail.com>
2024-04-25 12:59:53 +0330

    discount fixed

--- src/pages/cart/CartItem.jsx
+++ src/pages/cart/CartItem.jsx
@@ -1,6 +1,7 @@
 import styles from "./CartItem.module.css";
 
 function CartItem({ name, image, price, discount }) {
+  console.log(name);
   return (
     <div className={styles.container}>
       <div>
@@ -8,7 +9,9 @@ function CartItem({ name, image, price, discount }) {
       </div>
       <div className={styles.infoBox}>
         <span className={styles.name}>{name}</span>
-        <span className={styles.discount}>تحفیف: {discount}</span>
+        <span className={styles.discount}>
+          تخفیف: {discount == null ? "ندارد" : discount}
+        </span>
         <span className={styles.price}>قیمت : {price}</span>
       </div>
     </div>
