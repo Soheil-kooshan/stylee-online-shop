697f925b7999ef0a607d1d3fbabc9f264cb95dd2
soheil <soheilkooshan710@gmail.com>
2024-04-26 12:32:26 +0330

    linked to home

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
 function Logo() {
   return (
-    <img
-      src="src/assets/logo/stylee.svg"
-      alt="stylee logo"
-      className={styles.logo}
-    />
+    <Link to={"/"}>
+      <img
+        src="src/assets/logo/stylee.svg"
+        alt="stylee logo"
+        className={styles.logo}
+      />
+    </Link>
   );
 }
 
