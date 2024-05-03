f1666293f73c5d7795788506cd5013c90199cc84
soheil <soheilkooshan710@gmail.com>
2024-05-03 19:15:31 +0330

    persistor added

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,15 @@ import "./style/style.css";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist";
+import { persistor } from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
