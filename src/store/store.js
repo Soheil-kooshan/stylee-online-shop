37cdae36fba77e1f4fb680a6076447dbfcefe9b2
soheil <soheilkooshan710@gmail.com>
2024-05-03 19:08:53 +0330

    redux persist added to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./productsSlice";
 import cartReducer from "./cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  productReducer,
+  cartReducer
+);
 
 const store = configureStore({
-  reducer: {
-    products: productReducer,
-    cart: cartReducer,
-  },
+  reducer: persistReducer,
+  devTools: process.env.NODE_ENV !== "produvtion",
+  middleware: thunk,
 });
 
-export default store;
+export const persist = persistStore(store);
